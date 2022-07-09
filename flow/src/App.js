a2e95e54472a8b20c4d5ae432f73e66dc12d52a0
Zander <alexlamarche90@gmail.com>
2022-07-08 21:27:47 -0400

    creating resize

--- flow/src/App.js
+++ flow/src/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from "react";
+import Flow from "./components/Flow";
 
 class App extends Component{
+
   render(){
+    const mainStyle = {
+      width:"100%",
+      height:"100%",
+      position:"absolute",
+      backgroundColor:"gray",
+    };
+
     return (
-      <div>
-        <h1>Main</h1>
+      <div className="main-body" style={mainStyle}>
+        <Flow x={100} y={100}>
+          <h1>Alex Lamarche</h1>
+        </Flow>
       </div>
     );
   }
---
+++ flow/src/components/Flow.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+
+import "./Flow.css";
+
+export default class Flow extends Component{
+    constructor(props){
+        super(props);
+        this.x = props.x;
+        this.y = props.y;
+        this.width = props.width;
+        this.height = props.height;
+        this.children = props.children;
+    }
+
+    render(){
+
+        const styleWithChildren = {
+            width: "fit-content",
+            height: "fit-content",
+            padding:"10px",
+            position:"absolute",
+            top:this.y,
+            left:this.x,
+            backgroundColor:"lightgray"
+        };
+
+        return (
+            <div style={styleWithChildren}>
+                <Resize />
+                {this.children}
+            </div>
+        );
+    }
+}
+
+export class Resize extends Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return(
+            <div className="resize">
+                <div className="left-top"></div>
+                <div className="right-top"></div>
+                <div className="right-bottom"></div>
+                <div className="left-bottom"></div>
+            </div>
+        );
+    }
+}
