65a5540819c1511ce05e0ce00fe7c230d95e485d
Zander <alexlamarche90@gmail.com>
2022-07-09 00:10:03 -0400

    drag and drop working

--- flow/src/App.js
+++ flow/src/App.js
@@ -1,9 +1,27 @@
-import React, { Component } from "react";
+import React, { Component, useRef } from "react";
 import Flow from "./components/Flow";
+
+import {useDispatch} from "react-redux";
 
 class App extends Component{
 
+  constructor(props){
+    super(props);
+
+    // focused is the current Flow node(s) we are touching
+    this.state = {
+      focused: [null],
+    }
+
+    this.doSomething = this.doSomething.bind(this);
+  }
+
+  doSomething(item){
+    console.log(item);
+  }
+
   render(){
+    
     const mainStyle = {
       width:"100%",
       height:"100%",
@@ -14,7 +32,12 @@ class App extends Component{
     return (
       <div className="main-body" style={mainStyle}>
         <Flow x={100} y={100}>
-          <h1>Alex Lamarche</h1>
+          <h1>Mushroom 1</h1>
+          <ul>
+            <li>Hair?</li>
+            <li>Spore print</li>
+            <li>Cap width</li>
+          </ul>
         </Flow>
       </div>
     );
--- flow/src/components/Flow.js
+++ flow/src/components/Flow.js
@@ -1,25 +1,54 @@
-import React, { Component } from "react";
+import React, { Component, useRef } from "react";
 
 import "./Flow.css";
 
 export default class Flow extends Component{
+
     constructor(props){
         super(props);
-        this.x = props.x;
-        this.y = props.y;
-        this.width = props.width;
-        this.height = props.height;
         this.children = props.children;
-
         this.state = {
-            clicked:false
+            clicked:false,
+            width: props.width,
+            height: props.height,
+            x:props.x,
+            y:props.y,
+            isMoving:false
         };
     }
 
-    clicked(){
+    // Used to get the Flow components width and height after mount
+    ref = node => node && this.setState({
+        width: node.getBoundingClientRect().width,
+        height: node.getBoundingClientRect().height
+    });
+
+    // Shows and hides the resize options
+    reveal = () =>{
         this.setState({
             clicked:!this.state.clicked
         });
+    }
+
+    startMove = () =>{
+        this.setState({
+            isMoving: true
+        });
+    }
+
+    endMove = () =>{
+        this.setState({
+            isMoving: false
+        });
+    }
+
+    move = (e) => {
+        if(this.state.isMoving){
+            this.setState({
+                x:e.clientX - this.state.width/2,
+                y:e.clientY - this.state.height/2,
+            });
+        }
     }
 
     render(){
@@ -29,31 +58,27 @@ export default class Flow extends Component{
             height: "fit-content",
             padding:"10px",
             position:"absolute",
-            top:this.y,
-            left:this.x,
+            top:this.state.y,
+            left:this.state.x,
             backgroundColor:"lightgray"
         };
 
         return (
-            <div style={styleWithChildren} onClick={this.clicked()}>
-                {this.state.clicked ? <Resize /> : <div></div>}
-                {this.children}
-            </div>
-        );
-    }
-}
+            <div ref={this.ref} style={styleWithChildren} onClick={this.reveal} onMouseMove={this.move} onMouseDown={this.startMove} onMouseUp={this.endMove}>
 
-export class Resize extends Component{
-    constructor(props){
-        super(props)
-    }
-    render(){
-        return(
-            <div className="resize">
-                <div className="left-top"></div>
-                <div className="right-top"></div>
-                <div className="right-bottom"></div>
-                <div className="left-bottom"></div>
+                {/* The resizing buttons for controlling size*/}
+                {this.state.clicked ? 
+                    <div className="resize">
+                        <div className="left-top"></div>
+                        <div className="right-top"></div>
+                        <div className="right-bottom"></div>
+                        <div className="left-bottom"></div>
+                    </div>
+                : <div></div>}
+
+                {/* The nested content of the Flow class */}
+                {this.children}
+
             </div>
         );
     }
